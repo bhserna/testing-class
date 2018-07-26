d4fd678ff78578ef7d5bd44bbf4406f7cb2643db
bhserna <bhserna@gmail.com>
2018-07-26 18:56:06 -0500

    remove not used test file

--- 02-test-pure-functions/get-top-five.test.js
+++
@@ -1,9 +0,0 @@
-const { getTopFive } = require("./movies");
-
-describe("Get top five", () => {
-  test("no movies", () => {
-    const movies = [];
-    const top = getTopFive(movies);
-    expect(top).toEqual([]);
-  });
-});
